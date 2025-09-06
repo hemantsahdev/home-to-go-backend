b91fe16959e0b50e7c0979c008eff04b9b3f7686
hemantsahdev <hemantsahdevbusiness@gmail.com>
2025-09-06 14:11:25 +0530

    backend updated

--- index.js
+++ index.js
@@ -20,9 +20,7 @@ mongoose.connection.on('connected', () => {
 // for body parsing
 app.use(express.json());
 
-
 app.use("/" , userAuth)
-
 
 // STRIPE
 const stripe=require("stripe")(process.env.STRIPE_SECRET_KEY) 
@@ -71,19 +69,5 @@ app.post('/create-checkout-session',async(req,res)=>{
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(4000,()=>{
-    console.log("connected to port 4000");
-    
-})
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
